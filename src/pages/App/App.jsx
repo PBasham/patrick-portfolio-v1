c8f584982713f8d20add37e69e661c1ef53bc711
patrickbasham <PatrickTBasham@gmail.com>
2022-07-13 18:43:22 -0400

    set up initial file structure with homepage

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,16 @@
+/*========================================
+        Import dependencies
+========================================*/
+
+/*========================================
+        import styles
+========================================*/
+import "../../styles.css"
+
+export default function App() {
+  return (
+    <div className="App">
+        <h1>Test</h1>
+    </div>
+  );
+}
