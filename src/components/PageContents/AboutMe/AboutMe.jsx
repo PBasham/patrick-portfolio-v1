d8a0d187dfe47874dc5dd81db082d51990c4a72b
patrickbasham <PatrickTBasham@gmail.com>
2022-07-14 10:31:13 -0400

    updated styling for content-div and added img to about me section

--- src/components/PageContents/AboutMe/AboutMe.jsx
+++ src/components/PageContents/AboutMe/AboutMe.jsx
@@ -5,8 +5,9 @@ export default function AboutMe() {
         <div id="about" className="href-div">
             <div className="content-div">
                 <h1>About Me</h1>
-                <div className="profile=pic-two-wrapper" ></div>
-                <div id="profile-pic-two"></div>
+                <div className="profile-pic-two-wrapper" >
+                    <div id="profile-pic-two"></div>
+                </div>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
         </div>
