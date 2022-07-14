925dcca415535dfc660d2ff0774e2efd570343c0
patrickbasham <PatrickTBasham@gmail.com>
2022-07-14 09:10:29 -0400

    added styling for page-content cards and page is aligning correctly

--- src/components/HeaderArea/HeaderArea.jsx
+++ src/components/HeaderArea/HeaderArea.jsx
@@ -7,7 +7,7 @@ export default function HeaderArea() {
         <div className="header-area-wrapper">
             <div className="header-div">
                 <h1 id="profile-name">Hello, I'm Patrick Basham</h1>
-                <h3 id="profile-tagline">Software Engineer</h3>
+                <h3 id="profile-tagline">Software Engineer...</h3>
                 <img src="public/imgs/profile_pic.jpg" alt="" />
             </div>
         </div>
