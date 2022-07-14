f9bbd10c18507849904107e834a43bcb25197ced
patrickbasham <PatrickTBasham@gmail.com>
2022-07-13 20:06:29 -0400

    added state for navbar and updated styling

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function NavBar() {
+    
+  return (
+    <div className='nav-bar'></div>
+  )
+}
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -10,15 +10,44 @@ import HomePage from "../HomePage/HomePage";
         import styles
 ========================================*/
 import "../../styles.css"
+import NavBar from "../../components/NavBar/NavBar";
 
 export default function App() {
 
+    const [ navBarState, setNavBarState ] = useState({
+        activeLink: null,
+        links: [
+            {
+                id: 0,
+                href: "#content-about",
+            },
+            {
+                id: 1,
+                href: "#content-skills",
+            },
+            {
+                id: 2,
+                href: "#content-projects",
+            },
+            {
+                id: 3,
+                href: "#content-contact",
+            },
+        ]
+    })
+
+
+    /*========================================
+            Functions Start
+    ========================================*/
     
-    
-    
-  return (
-    <div className="App">
-        <HomePage />
-    </div>
-  );
+    /* Functions END */
+
+    return (
+        <div className="App">
+            <NavBar navBarState={navBarState} />
+            <HomePage />
+            <div className="footer-div"></div>
+        </div>
+    );
 }
