1e52c6cdb037d4b10fa57c12d8b12704e4be776a
patrickbasham <PatrickTBasham@gmail.com>
2022-07-14 04:22:30 -0400

    set up base page content divs

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,12 +6,25 @@ import React from 'react'
         import pages & components
 ========================================*/
 import HeaderArea from "../../components/HeaderArea/HeaderArea";
+import AboutMe from "../../components/PageContents/AboutMe/AboutMe.jsx"
+import Skills from "../../components/PageContents/Skills/Skills.jsx"
+import Projects from "../../components/PageContents/Projects/Projects.jsx"
+import Contact from "../../components/PageContents/Contact/Contact.jsx"
+/*========================================
+        Import Styling
+========================================*/
+import "../../components/PageContents/PageContents.css"
 
 export default function HomePage() {
-  return (
-    <div>
-        <HeaderArea />
-        This is the homepage!
-    </div>
-  )
+    return (
+        <div>
+            <HeaderArea />
+            <div className="content-wrapper">
+                <AboutMe />
+                <Skills />
+                <Projects />
+                <Contact />
+            </div>
+        </div>
+    )
 }
