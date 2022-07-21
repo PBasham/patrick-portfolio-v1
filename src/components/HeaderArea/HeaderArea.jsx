0a5f5f606ff470f62cc20c77c42168d219a29a41
patrickbasham <PatrickTBasham@gmail.com>
2022-07-21 17:40:55 -0400

    updated styling for page and added profile image to header

--- src/components/HeaderArea/HeaderArea.jsx
+++ src/components/HeaderArea/HeaderArea.jsx
@@ -7,12 +7,12 @@ import "./HeaderArea.css"
 export default function HeaderArea() {
     return (
         <div className="header-area-wrapper">
+            <img className="profile-img" />
             <div className="header-div">
-                <img src="public/imgs/profile_pic.jpg" alt="" />
                 <h1 id="profile-name">Hello,</h1>
                 <h3>My name is Patrick Basham.</h3>
                 <h3>I am located in Queens, NY,</h3>
-                    <h3>and I...</h3>
+                <h3>and I...</h3>
                 <div className="typed-text-div">
                     <h3 className="typed-text">
                         <Typewriter
@@ -29,7 +29,7 @@ export default function HeaderArea() {
                         <i class="fa fa-github"></i>
                     </a>
                     <a id="link-linkedin" className="profile-link" href="https://www.linkedin.com/in/patrickbasham/" target="_blank">
-                    <i class="fa fa-linkedin"></i>
+                        <i class="fa fa-linkedin"></i>
                     </a>
                 </div>
             </div>
