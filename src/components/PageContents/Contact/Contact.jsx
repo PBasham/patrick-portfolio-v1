bd90141c280b50ad39acac25a6dab7bb7135018c
patrickbasham <PatrickTBasham@gmail.com>
2022-07-21 17:21:22 -0400

    added styling for skills and started base for contact form

--- src/components/PageContents/Contact/Contact.jsx
+++ src/components/PageContents/Contact/Contact.jsx
@@ -5,7 +5,13 @@ export default function Contact() {
 
         <div id="contact" className="href-div">
             <div className="content-div">
-                <h1>Contact</h1>
+                <h1>Contact Me</h1>
+                <form className="contact-form" action="https://formsubmit.co/patricktbasham@email.com" method="POST">
+                    <input id="name" type="text" name="name" placeholder="name"/>
+                    <input id="email" type="email" name="email" placeholder="your email"/>
+                    <textarea id="message" name="message" rows="4" placeholder="message"></textarea>
+                    <button type="submit">Send Email</button>
+                </form>
             </div>
         </div>
     )
--- src/components/PageContents/Skills/Skills.jsx
+++ src/components/PageContents/Skills/Skills.jsx
@@ -12,44 +12,69 @@ export default function Skills() {
                     <div className="skill-list skills-front-end">
                         <h3>Front-End</h3>
                         <ul>
-                            <i class="fa-brands fa-html5"></i>
-                            <li>HTML5</li>
-                            <i class="fa-brands fa-css3-alt"></i>
-                            <li>CSS3</li>
-                            {/* <FontAwesomeIcon icon="fa-brands fa-react" /> */}
-                            <i class="fa-brands fa-react"></i>
-                            <li>React</li>
-                            <i class="fa-brands fa-js"></i>
-                            <li>JavaScript</li>
+                            <li>
+                                <i class="fa-brands fa-html5"></i>
+                                HTML5
+                            </li>
+                            <li>
+                                <i class="fa-brands fa-css3-alt"></i>
+                                CSS3
+                            </li>
+                            <li>
+                                <i class="fa-brands fa-react"></i>
+                                React
+                            </li>
+                            <li>
+                                <i class="fa-brands fa-js"></i>
+                                JavaScript
+                            </li>
 
                         </ul>
                     </div>
                     <div className="skill-list skills-tools">
                         <h3>Tools</h3>
                         <ul>
-                            <i class="fa-brands fa-git-alt"></i>
-                            <li>Git</li>
-                            <i class="devicon-heroku-original colored"></i>
-                            <li>Heroku</li>
+                            <li>
+                                <i class="fa-brands fa-git-alt"></i>
+                                Git
+                            </li>
+                            <li>
+                                <i class="devicon-heroku-original colored"></i>
+                                Heroku
+                            </li>
                         </ul>
                     </div>
                     <div className="skill-list skills-back-end">
                         <h3>Back-End</h3>
                         <ul>
-                            <i class="fa-brands fa-node"></i>
-                            <li>Node.js</li>
-                            <i class="devicon-express-original"></i>
-                            <li>Express</li>
-                            <i class="fa-solid fa-cloud"></i>
-                            <li>RESTful API</li>
-                            <i class="fa-solid fa-leaf"></i>
-                            <li>MongoDB</li>
-                            <i class="fa-solid fa-database"></i>
-                            <li>SQL</li>
-                            <i class="devicon-postgresql-plain-wordmark colored"></i>
-                            <li>PostgressSQL</li>
-                            <i class="fa-brands fa-python"></i>
-                            <li>Python</li>
+                            <li>
+                                <i class="fa-brands fa-node"></i>
+                                Node.js
+                            </li>
+                            <li>
+                                <i class="devicon-express-original"></i>
+                                Express
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-cloud"></i>
+                                RESTful API
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-leaf"></i>
+                                MongoDB
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-database"></i>
+                                SQL
+                            </li>
+                            <li>
+                                <i class="devicon-postgresql-plain-wordmark colored"></i>
+                                PostgressSQL
+                            </li>
+                            <li>
+                                <i class="fa-brands fa-python"></i>
+                                Python
+                            </li>
                         </ul>
                     </div>
                 </div>
