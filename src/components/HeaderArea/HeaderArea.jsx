8ba2a262b0046853a569421a08cb7f6fe5ce8650
patrickbasham <PatrickTBasham@gmail.com>
2022-07-21 10:56:49 -0400

    updated styling

--- src/components/HeaderArea/HeaderArea.jsx
+++ src/components/HeaderArea/HeaderArea.jsx
@@ -6,9 +6,11 @@ export default function HeaderArea() {
     return (
         <div className="header-area-wrapper">
             <div className="header-div">
-                <h1 id="profile-name">Hello, I'm Patrick Basham</h1>
-                <h3 id="profile-tagline">Software Engineer...</h3>
                 <img src="public/imgs/profile_pic.jpg" alt="" />
+                <h1 id="profile-name">Hello,</h1>
+                <h3>My name is Patrick Basham.</h3>
+                <h3>I am located in Queens, NY.</h3>
+                <h3 className="typed-text">I am ...</h3>
             </div>
             <div className="animation-div"></div>
         </div>
