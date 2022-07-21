2d403d09ad8ba20f3220176c957ffe177c9889cc
patrickbasham <PatrickTBasham@gmail.com>
2022-07-21 14:21:17 -0400

    added icons and updated positioning / styling for header-div

--- src/components/HeaderArea/HeaderArea.jsx
+++ src/components/HeaderArea/HeaderArea.jsx
@@ -12,27 +12,25 @@ export default function HeaderArea() {
                 <h1 id="profile-name">Hello,</h1>
                 <h3>My name is Patrick Basham.</h3>
                 <h3>I am located in Queens, NY,</h3>
+                    <h3>and I...</h3>
                 <div className="typed-text-div">
-                    <h3>and I</h3><h3 className="typed-text">
-
-                    <Typewriter
-                        onInit={(typewriter) => {
-                            // typewriter.typeString("a software engineer.")
-                            // .pauseFor(1000)
-                            // .deleteAll()
-                            // .pauseFor(1000)
-                            // typewriter.typeString("always looking to learn more.")
-                            // .pauseFor(1000)
-                            // .deleteAll()
-                            // .start();
-                        }}
-                        options={{
-                            strings: ["am a software engineer.", "am always looking to learn more.", "am highly motivated.", "love to code!"],
-                            autoStart: true,
-                            loop: true,
-                        }}
+                    <h3 className="typed-text">
+                        <Typewriter
+                            options={{
+                                strings: ["am a software engineer.", "am always looking to learn more.", "am highly motivated.", "love to code!"],
+                                autoStart: true,
+                                loop: true,
+                            }}
                         />
-                        </h3>
+                    </h3>
+                </div>
+                <div className="profile-links">
+                    <a id="link-github" className="profile-link" href="https://github.com/PBasham" target="_blank">
+                        <i class="fa fa-github"></i>
+                    </a>
+                    <a id="link-linkedin" className="profile-link" href="https://github.com/PBasham" target="_blank">
+                    <i class="fa fa-linkedin"></i>
+                    </a>
                 </div>
             </div>
             <div className="animation-div"></div>
