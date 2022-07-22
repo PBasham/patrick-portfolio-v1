beee5ecada24d573ee00a1820b4797d88db9aa22
patrickbasham <PatrickTBasham@gmail.com>
2022-07-22 17:26:42 -0400

    udpated about

--- src/components/PageContents/AboutMe/AboutMe.jsx
+++ src/components/PageContents/AboutMe/AboutMe.jsx
@@ -20,7 +20,7 @@ export default function AboutMe() {
                 <div className="about-picture">
                 </div>
                 <div className="content-about">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p>I'm a Software Engineer based in New York with a background in electrical construction. After almost 5 years in the electrical industry from the warehouse to the field then the office, I finally took a leap with a lot of help from my wife to leave my job and go towards something I felt I would be truly passionate about. I'm always trying to find more efficent ways to do things and always welcome the challange of learning somehting new. Becoming a software engineer has given me the oppurtunity to always learn and advance my skills to overcome challanges that come my way.</p>
                 </div>
             </div>
         </div>
