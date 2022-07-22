ab861c20c84f54d2d129ac1bef25c66623c6a21f
patrickbasham <PatrickTBasham@gmail.com>
2022-07-21 20:30:09 -0400

    added fuunctionality to form

--- src/components/PageContents/Contact/Contact.jsx
+++ src/components/PageContents/Contact/Contact.jsx
@@ -6,11 +6,11 @@ export default function Contact() {
         // <div id="contact" className="href-div">
             <div id="contact" className="content-div">
                 <h1>Contact Me</h1>
-                <form className="contact-form" action="https://formsubmit.co/patricktbasham@email.com" method="POST">
-                    <input id="name" type="text" name="name" placeholder="name"/>
+                <form className="contact-form" action="https://formsubmit.co/1297a0fa1a7f93e4f761fce3583a363c" method="POST">
                     <input id="email" type="email" name="email" placeholder="your email"/>
+                    <input id="name" type="text" name="name" placeholder="name"/>
                     <textarea id="message" name="message" rows="4" placeholder="message"></textarea>
-                    <button type="submit">Send Email</button>
+                    <button className="shake" type="submit">Send Email</button>
                 </form>
             </div>
         // </div>
