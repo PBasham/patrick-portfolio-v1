0dada58258fdb92d6dfca661fc0245f315d25e6a
patrickbasham <PatrickTBasham@gmail.com>
2022-07-22 01:38:28 -0400

    updated spacing in footer and enlarged textarea on contact form

--- src/components/PageContents/Contact/Contact.jsx
+++ src/components/PageContents/Contact/Contact.jsx
@@ -9,7 +9,7 @@ export default function Contact() {
                 <form className="contact-form" action="https://formsubmit.co/1297a0fa1a7f93e4f761fce3583a363c" method="POST">
                     <input id="email" type="email" name="email" placeholder="your email"/>
                     <input id="name" type="text" name="name" placeholder="name"/>
-                    <textarea id="message" name="message" rows="4" placeholder="message"></textarea>
+                    <textarea id="message" name="message" rows="6" placeholder="message"></textarea>
                     <button className="shake" type="submit">Send Email</button>
                 </form>
             </div>
